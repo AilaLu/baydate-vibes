dd53cf1f9578697cd8688621b0acb7e6894c0297
Aila Lu <ailalutw@gmail.com>
2025-06-03 17:38:45 -0700

    dateFilter shows mood and energy, needs progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import DateFilter from './components/dateFilter';
 
 function App() {
   return (
+    
     <main className="min-h-screen bg-pink-50 text-gray-900">
       <DateFilter />
     </main>
+    
   );
 }
 
